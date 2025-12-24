dce78ac69d470d02ffdcc62d9820551047fe4130
Aneri252 <124673018+Aneri252@users.noreply.github.com>
2025-12-24 18:37:42 +0530

    setup staging db

--- db.php
+++ db.php
@@ -6,6 +6,12 @@ if (isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] === 'localhost' || $_
     $db_pass = "";
     $db_name = "moron";
     
+} else if (isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] === 'https://stage.morsan.co.in' || $_SERVER['HTTP_HOST'] === 'stage.morsan.co.in')) {
+    // Staging database settings
+    $db_host = "localhost";
+    $db_user = "u696140658_morsan_stage";
+    $db_pass = "Morsan@2025";
+    $db_name = "u696140658_morsan_stage";
 } else {
     // Server (production) database settings
     $db_host = "localhost";
