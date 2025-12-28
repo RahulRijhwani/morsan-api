81869330e474827187b860621b0199426a20a5d4
Aneri252 <124673018+Aneri252@users.noreply.github.com>
2025-12-28 17:01:14 +0530

    changes for stage url

--- db.php
+++ db.php
@@ -1,24 +1,26 @@
 <?php
-if (isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1')) {
-    // Local database settings
-    $db_host = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "moron";
+// if (isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1')) {
+//     // Local database settings
+//     $db_host = "localhost";
+//     $db_user = "root";
+//     $db_pass = "";
+//     $db_name = "moron";
     
-} else if (isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] === 'https://stageapi.morsan.co.in/' || $_SERVER['HTTP_HOST'] === 'stageapi.morsan.co.in')) {
+// } else 
+if (isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] === 'https://stageapi.morsan.co.in/' || $_SERVER['HTTP_HOST'] === 'stageapi.morsan.co.in')) {
     // Staging database settings
     $db_host = "localhost";
     $db_user = "u696140658_morsan_stage";
     $db_pass = "Morsan@2025";
     $db_name = "u696140658_morsan_stage";
-} else {
-    // Server (production) database settings
-    $db_host = "localhost";
-    $db_user = "u696140658_morsan";
-    $db_pass = "morsan@DB12";
-    $db_name = "u696140658_morsan";
-}
+} 
+// else {
+//     // Server (production) database settings
+//     $db_host = "localhost";
+//     $db_user = "u696140658_morsan";
+//     $db_pass = "morsan@DB12";
+//     $db_name = "u696140658_morsan";
+// }
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_error) {
